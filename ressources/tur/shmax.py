6e2b5f7ee9ed6298f1e4b86ea281b9880aa82da0
phpxml <127610041+phpxml@users.noreply.github.com>
2026-01-24 14:27:51 +0100

    Update shmax.py

--- ressources/tur/shmax.py
+++ ressources/tur/shmax.py
@@ -5,14 +5,14 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-def get_token_link():
+def get_link():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
-    options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
-    
+    options.add_argument("--window-size=1920,1080") # Sayfayı tam boy açalım
+
     try:
         driver = webdriver.Chrome(
             service=Service(ChromeDriverManager().install()),
@@ -20,26 +20,36 @@ def get_token_link():
         )
         
         driver.get("https://www.showmax.com.tr/canli-yayin")
-        time.sleep(30) # Token'ın oluşması için gereken süre
-
+        
+        # Linki yakalamak için 45 saniye boyunca her 5 saniyede bir kontrol et
         found_url = None
-        for request in reversed(driver.requests):
-            if request.response:
-                if ".m3u8" in request.url and "ciner" in request.url:
-                    found_url = request.url
-                    break
+        for _ in range(9): 
+            time.sleep(5)
+            for request in reversed(driver.requests):
+                if request.response:
+                    url = request.url.lower()
+                    if ".m3u8" in url and "ciner" in url:
+                        found_url = request.url
+                        break
+            if found_url:
+                break
         
         driver.quit()
         return found_url
-    except Exception as e:
+    except:
         return None
 
 if __name__ == "__main__":
-    final_link = get_token_link()
-    if final_link:
+    live_url = get_link()
+    
+    # EĞER LİNK BULUNURSA YAZDIR
+    if live_url:
         print("#EXTM3U")
         print("#EXTINF:-1,Show Max")
-        print(final_link)
-        sys.stdout.flush()
+        print(live_url)
+        sys.stdout.flush() # Veriyi hemen dosyaya gönder
     else:
-        sys.exit(1) # Link bulunamazsa hata kodu ver
+        # Link bulunamazsa bile dosya boş kalmasın diye sabit bir mesaj yazdırabiliriz
+        # Ya da hata verdirerek GitHub'ın KIRMIZI yanmasını sağlayalım ki durumu anla
+        sys.stderr.write("Link yakalanamadı!\n")
+        sys.exit(1) 
